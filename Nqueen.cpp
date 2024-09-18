4d298fdd18a6e7c6d530e3447bf4a17330b2ec7d
Devansh Mishra <115351387+dkmishra2407@users.noreply.github.com>
2024-09-18 02:40:56 -0700

    Updated Date 17Sept

--- Nqueen.cpp
+++ Nqueen.cpp
@@ -1,58 +1,94 @@
-#include <vector>
-#include <cmath>
-#include <iostream>
+// #include <vector>
+// #include <cmath>
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-vector<vector<string>> ans;
+// vector<vector<string>> ans;
 
-bool place(const vector<int>& s, int col, int row) {
-    for (int j = 0; j < row; j++) {
-        if (s[j] == col || abs(s[j] - col) == abs(j - row)) {
-            return false;
-        }
-    }
-    return true;
-}
+// bool place(const vector<int>& s, int col, int row) {
+//     for (int j = 0; j < row; j++) {
+//         if (s[j] == col || abs(s[j] - col) == abs(j - row)) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
-void nqueen(vector<int>& s, int n, int row) {
-    if (row == n) {
-        vector<string> board(n, string(n, '.'));
-        for (int i = 0; i < n; i++) {
-            board[i][s[i]] = 'Q';
-        }
-        ans.push_back(board);
-        return;
-    }
+// void nqueen(vector<int>& s, int n, int row) {
+//     if (row == n) {
+//         vector<string> board(n, string(n, '.'));
+//         for (int i = 0; i < n; i++) {
+//             board[i][s[i]] = 'Q';
+//         }
+//         ans.push_back(board);
+//         return;
+//     }
 
-    for (int col = 0; col < n; col++) {
-        if (place(s, col, row)) {
-            s[row] = col;
-            nqueen(s, n, row + 1);
-        }
-    }
-}
+//     for (int col = 0; col < n; col++) {
+//         if (place(s, col, row)) {
+//             s[row] = col;
+//             nqueen(s, n, row + 1);
+//         }
+//     }
+// }
 
-vector<vector<string>> solveNQueens(int n) {
-    vector<int> store_col(n, 0);
-    nqueen(store_col, n, 0);
-    return ans;
-}
+// vector<vector<string>> solveNQueens(int n) {
+//     vector<int> store_col(n, 0);
+//     nqueen(store_col, n, 0);
+//     return ans;
+// }
 
-int main() {
-    int n = 8;
-    auto results = solveNQueens(n);
+// int main() {
+//     int n = 8;
+//     auto results = solveNQueens(n);
     
-    for (const auto& board : results) {
-        for (const auto& row : board) {
-            cout << row << endl;
-        }
-        cout << endl;
+//     for (const auto& board : results) {
+//         for (const auto& row : board) {
+//             cout << row << endl;
+//         }
+//         cout << endl;
+//     }
+
+//     return 0
+// }
+    
+    bool isSafe(vector<vector<bool>> &board, int n, int r, int c) {
+        for (int i = 0; i < r; i++)
+            if (board[i][c]) return false;
+        for (int i = r, j = c; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j]) return false;
+        for (int i = r, j = c; i >= 0 && j < n; i--, j++)
+            if (board[i][j]) return false;
+        return true;
     }
-
-    return 0;
-}
-
+    
+    void find_positions(vector<vector<bool>> &board, vector<int> &queen_positions, vector<vector<int>> &ans, int n, int r) {
+        if (r == n) {
+            ans.push_back(queen_positions);
+            return;
+        }
+    
+        for (int c = 0; c < n; c++) {
+            if (isSafe(board, n, r, c)) {
+                board[r][c] = true;
+                queen_positions.push_back(c + 1);
+                find_positions(board, queen_positions, ans, n, r + 1);
+                queen_positions.pop_back();
+                board[r][c] = false;
+            }
+        }
+    }
+    
+    vector<vector<int>> nQueen(int n) {
+        vector<vector<bool>> board(n, vector<bool>(n, false));
+        vector<vector<int>> ans;
+        vector<int> queen_positions;
+    
+        find_positions(board, queen_positions, ans, n, 0);
+    
+        return ans;
+    }
 
 // SOLUTION 2
 
